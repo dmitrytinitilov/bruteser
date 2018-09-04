b9dd0fd72ad2286fe63b69f1d8f996d9a94fd3d2
Dmitry Tinitilov <dmitry.tinitilov@gmail.com>
2018-09-04 19:34:19 +0300

    moving server to express

--- server.js
+++ server.js
@@ -3,45 +3,21 @@ var url  = require('url');
 var fs = require('fs');
 var path = require('path');
 
-var server = http.createServer(function(req, res) {
-	
-	var filepath = req.url;
-	if (filepath=='/') {
-		var filepath = '/index.html';
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+
+(async function() {
+
+	try {
+		var server = app.listen(8080,function(){
+			var host = server.address().address;
+			var port = server.address().port;
+
+			console.log("Server is running on port http://%s:%s", host, port)
+		})
+	} catch(e) {
+		console.log(e);
 	}
 
-	var ext = path.extname(filepath);
-
-	switch (ext) {
-		case '.html':
-			res.setHeader('content-type', 'text/html');
-		break;
-		case '.css':
-			res.setHeader('content-type', 'text/css');
-		break;
-	}
-	
-	
-	res.writeHead(200);
-
-	console.log(filepath);
-
-	fs.readFile('public'+filepath, function (err, data){
-		if (err) {
-			if (filepath === '/index.html') {
-				res.end("It seems there is no index.html file in 'public' directory");
-			} else {
-				res.end("There is no file by this address");
-			}
-
-			
-
-		}
-		res.end(data);
-	});
-
-});
-
-server.listen(8080);
-
-console.log("Server is running on port 8080");
+})()
